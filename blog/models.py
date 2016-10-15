71138f1c8a636a387aaed8a856cbd8b881e250e7
Sashka12345 <pljut@ukr.net>
2016-10-15 15:11:46 +0300

    izmenenie v post_list.html

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+
+
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
@@ -9,5 +11,11 @@ class Post(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
-def __str__(self):
-    return self.title
+
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
